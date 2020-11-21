f28e76ca229e9842099310c393091e66fbe0c633
Jaffer Ali <jafferali150@gmail.com>
2020-11-22 00:29:43 +0530

    added js engine revised arrow bg animation.cs html

---
+++ js/engine.js
@@ -0,0 +1,10 @@
+function encode() {
+	var obj = document.getElementById('rawstring');
+	var unencoded = obj.value;
+	obj.value = encodeURIComponent(unencoded).replace(/'/g,"%27").replace(/"/g,"%22");	
+}
+function decode() {
+	var obj = document.getElementById('rawstring');
+    var encoded = obj.value;
+	obj.value = decodeURIComponent(encoded.replace(/\+/g,  " "));
+}
